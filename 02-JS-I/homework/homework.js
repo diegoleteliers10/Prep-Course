06c68c7abd3252b64e0f011d6782da27ca4835ee
diegoleteliers10 <diegoleteliers10@gmail.com>
2023-01-08 18:09:28 -0300

    ready

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -142,13 +142,13 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return Math.pow(2)
+  return Math.pow(num,2)
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return Math.pow(3)
+  return Math.pow(num,3)
 }
 
 function elevar(num, exponent) {
